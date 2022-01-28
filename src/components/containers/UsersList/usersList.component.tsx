419b3d352c509ce06f2a7199d4b6b79b112108bd
y.troniak <y.troniak@levi9.com>
2022-01-28 16:18:12 +0200

    skeleton

--- src/components/containers/UsersList/usersList.component.tsx
+++ src/components/containers/UsersList/usersList.component.tsx
@@ -6,6 +6,8 @@ import {
   ListItemText,
   Typography,
   Divider,
+  Box,
+  Skeleton,
 } from "@mui/material";
 import React, { FC, Fragment } from "react";
 import { UserProps } from "./usersList.container";
@@ -14,8 +16,29 @@ interface ListWrapperProps {
   users: UserProps[];
 }
 export const UsersList: FC<ListWrapperProps> = ({ users }) => {
+  if (!users.length) {
+    return (
+      <List sx={{ width: 240 }}>
+        {Array(5).fill(5).map((_,i) => (
+          <ListItem alignItems="flex-start" key={i}>
+            <ListItemAvatar>
+              <Skeleton variant="circular" width={40} height={40} />
+            </ListItemAvatar>
+            <ListItemText
+              primary={<Skeleton variant="text" />}
+              secondary={
+                <Typography component="span" variant="body2">
+                  <Skeleton variant="text" />
+                </Typography>
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+    );
+  }
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    <List sx={{ width: 240, bgcolor: "background.paper" }}>
       {users.map((user) => (
         <Fragment key={user.id}>
           <ListItem alignItems="flex-start">
@@ -25,23 +48,13 @@ export const UsersList: FC<ListWrapperProps> = ({ users }) => {
             <ListItemText
               primary={user.name}
               secondary={
-                <>
-                  <Typography
-                    component="p"
-                    variant="body2"
-                    color={user.isActive ? "#007f5f" : "#f08080"}
-                  >
-                    {user.isActive ? "Online" : "Offline"}
-                  </Typography>
-                  <Typography
-                    component="p"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    lon:{user.longitude}
-                    lat:{user.latitude}
-                  </Typography>
-                </>
+                <Typography
+                  component="span"
+                  variant="body2"
+                  color={user.isActive ? "#007f5f" : "#f08080"}
+                >
+                  {user.isActive ? "Online" : "Offline"}
+                </Typography>
               }
             />
           </ListItem>

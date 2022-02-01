2ed799dc7c04226d3b3a3376a7be63bb3f18dac9
y.troniak <y.troniak@levi9.com>
2022-02-01 13:51:42 +0200

    adding deploy procces ci/cd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/your-github-repo-name/' : ''
 }
 
 module.exports = nextConfig

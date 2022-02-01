271c09e025d746b29232f02f2850c353a74ce3e9
y.troniak <y.troniak@levi9.com>
2022-02-01 14:06:07 +0200

    typo

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/your-github-repo-name/' : ''
+  assetPrefix: isProd ? '/whereiam/' : ''
 }
 
 module.exports = nextConfig

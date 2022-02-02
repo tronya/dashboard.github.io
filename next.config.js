26c0b0f1d2f8a4afe23e60a977630a296e0a2742
Nataliia Leshchak <n.leshchak@ualwo8689nbl.levi9.com>
2022-02-02 13:46:07 +0200

    remove unnecessary files, disable PWA in dev env

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@
 const withPWA = require("next-pwa");
 
 const isProd = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: isDev,
   },
   reactStrictMode: true,
   assetPrefix: isProd ? "/whereiam/" : "",

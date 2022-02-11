9d750e4fded740790b5912365ef2b94dd8725dc2
Troniak Yura <tronyakyura@@gmail.com>
2022-02-11 11:40:26 +0200

    adding esling

--- src/components/containers/User/userProfile.tsx
+++ src/components/containers/User/userProfile.tsx
@@ -1,73 +1,76 @@
-import {Avatar, Box, Button, Paper, Typography} from "@mui/material";
-import {FC} from "react";
-import {useTranslation} from "react-i18next";
-import {useAuth} from "../../../hooks/useAuth";
-import Breadcrumbs from "../../ui/Breadcrumbs/breadcrumbs";
-import PageTitle from "../../ui/PageTitle/PageTitle";
-import {stringAvatar} from "../../../utils/user";
-import Loader from "../../ui/Loader/loader";
-import {toast} from "react-toastify";
-import {useGeolocationProvider} from "../../../hooks/useGeolocationProvider";
+import { Avatar, Box, Button, Paper, Typography } from '@mui/material'
+import { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useAuth } from '../../../hooks/useAuth'
+import Breadcrumbs from '../../ui/Breadcrumbs/breadcrumbs'
+import PageTitle from '../../ui/PageTitle/PageTitle'
+import { stringAvatar } from '../../../utils/user'
+import Loader from '../../ui/Loader/loader'
+import { toast } from 'react-toastify'
+import { useGeolocationProvider } from '../../../hooks/useGeolocationProvider'
 
 const UserProfile: FC = () => {
-    const {t} = useTranslation();
-    const {user, loading} = useAuth();
-    const {status, isLocationAllowed} = useGeolocationProvider()
-    if (!user) {
-        return null;
-    }
-    if (loading) {
-        return <Loader/>
-    }
-    console.log(status, isLocationAllowed)
-    const setAcceptLocation = () => {
-        navigator.geolocation.getCurrentPosition((success) => {
-            console.log(success)
-        }, (e) => {
-            toast.error(e.message)
-        })
-    }
+  const { t } = useTranslation()
+  const { user, loading } = useAuth()
+  const { isLocationAllowed } = useGeolocationProvider()
+  if (!user) {
+    return null
+  }
+  if (loading) {
+    return <Loader />
+  }
+  const setAcceptLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        console.log(success)
+        //TODO some  callback should be to retrigger User (success.onchange)
+      },
+      (e) => {
+        toast.error(e.message)
+      }
+    )
+  }
 
-    return (
-        <>
-            <Breadcrumbs breadcrumbText={t("profile.title")}/>
-            <PageTitle title={t("profile.title")}/>
-            <Paper>
-                <Box display="flex" flexDirection="column" p={6} alignItems="center">
-                    {user.displayName && user.photoURL && (
-                        <Avatar
-                            alt={user.displayName}
-                            src={user.photoURL}
-                            {...(stringAvatar(user.displayName), {width: 120, height: 120})}
-                        />
-                    )}
-                    <Box display="flex" flexDirection="column" alignItems="center" mt={6}>
-                        <Typography variant="h6" sx={{color: "common.white"}}>
-                            {user.displayName}
-                        </Typography>
-                        <Typography variant="h6" sx={{color: "common.white"}}>
-                            {user.email}
-                        </Typography>
-                    </Box>
-                </Box>
-            </Paper>
-            {!isLocationAllowed && (
-                <Box width={1} mt={2}>
-                    <Button
-                        variant="contained"
-                        fullWidth
-                        sx={{
-                            color: "common.white",
-                            backgroundColor: "green !important",
-                        }}
-                        onClick={() => setAcceptLocation()}
-                    >
-                        {t("profile.allowLocationButtonText")}
-                    </Button>
-                </Box>
-            )}
-        </>
-    );
-};
+  return (
+    <>
+      <Breadcrumbs breadcrumbText={t('profile.title')} />
+      <PageTitle title={t('profile.title')} />
+      <Paper>
+        <Box display="flex" flexDirection="column" p={6} alignItems="center">
+          {user.displayName && user.photoURL && (
+            <Avatar
+              alt={user.displayName}
+              src={user.photoURL}
+              {...(stringAvatar(user.displayName), { width: 120, height: 120 })}
+            />
+          )}
+          <Box display="flex" flexDirection="column" alignItems="center" mt={6}>
+            <Typography variant="h6" sx={{ color: 'common.white' }}>
+              {user.displayName}
+            </Typography>
+            <Typography variant="h6" sx={{ color: 'common.white' }}>
+              {user.email}
+            </Typography>
+          </Box>
+        </Box>
+      </Paper>
+      {!isLocationAllowed && (
+        <Box width={1} mt={2}>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              color: 'common.white',
+              backgroundColor: 'green !important',
+            }}
+            onClick={() => setAcceptLocation()}
+          >
+            {t('profile.allowLocationButtonText')}
+          </Button>
+        </Box>
+      )}
+    </>
+  )
+}
 
-export default UserProfile;
+export default UserProfile
--- src/hooks/useNavigation.tsx
+++ src/hooks/useNavigation.tsx
@@ -1,22 +1,22 @@
-import { useRouter } from "next/router";
-import { createContext, FC, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
-import { useAuth } from "./useAuth";
-import { setUserGeolocationData } from "../../pages/api/usersGeolocation";
-import { UserGeolocation } from "../models/usersGeolocation";
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { toast } from 'react-toastify'
+import { setUserGeolocationData } from '../../pages/api/usersGeolocation'
+import { UserGeolocation } from '../models/usersGeolocation'
+import { useAuth } from './useAuth'
 
 export const useNavigation = (isLocationAllowed: boolean = false) => {
-  const router = useRouter();
-  const auth = useAuth();
-  const { t } = useTranslation();
+  const router = useRouter()
+  const auth = useAuth()
+  const { t } = useTranslation()
 
-  const [geolocation, setGeolocation] = useState<GeolocationPosition>();
+  const [geolocation, setGeolocation] = useState<GeolocationPosition>()
 
   useEffect(() => {
-      const watcher = navigator.geolocation.watchPosition(
+    if (isLocationAllowed) {
+      navigator.geolocation.getCurrentPosition(
         (success: GeolocationPosition) => {
-            console.log(success)
           const data: UserGeolocation = {
             coords: {
               accuracy: success.coords.accuracy,
@@ -30,21 +30,20 @@ export const useNavigation = (isLocationAllowed: boolean = false) => {
             timestamp: success.timestamp,
             displayName: auth.user?.displayName,
             photoURL: auth.user?.photoURL,
-            uid: auth.user?.uid ?? "",
-          };
-          setGeolocation(success);
-          setUserGeolocationData(auth.user?.uid, data);
+            uid: auth.user?.uid ?? '',
+          }
+          setGeolocation(success)
+          setUserGeolocationData(auth.user?.uid, data)
 
-          toast.success(t("toastSuccess.acceptGeolocation"));
-          router.push("/");
+          toast.success(t('toastSuccess.acceptGeolocation'))
+          router.push('/')
         },
         (error: GeolocationPositionError) => {
-          toast.error(error.message);
+          toast.error(error.message)
         }
-      );
+      )
+    }
+  }, [isLocationAllowed, router, t, auth])
 
-    return () => navigator.geolocation.clearWatch(watcher)
-  }, [isLocationAllowed, router, t, auth]);
-
-  return geolocation;
-};
+  return geolocation
+}

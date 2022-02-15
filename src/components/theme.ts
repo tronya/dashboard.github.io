25bbe2b84eb44e96c63979eb3be2e281c15037c6
Troniak Yura <tronyakyura@@gmail.com>
2022-02-15 10:44:50 +0200

    commens

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -4,18 +4,6 @@ const theme = createTheme({
   palette: {
     mode: 'dark',
   },
-  // palette: {
-  //   mode: 'light',
-  //   primary: {
-  //     main: '#264f9e',
-  //   },
-  //   secondary: {
-  //     main: '#455fb4',
-  //   },
-  //   background: {
-  //     paper: '#2d56a5',
-  //   },
-  // },
 });
 
 export default theme;

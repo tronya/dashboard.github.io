4bfeffb630054e37737bb19e7e3488ebbe448698
Nataliia Leshchak <n.leshchak@ualwo8689nbl.levi9.com>
2022-02-16 16:14:40 +0200

    mock

--- src/components/containers/Map/mapBox.component.tsx
+++ src/components/containers/Map/mapBox.component.tsx
@@ -1,14 +1,33 @@
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import { Paper } from '@mui/material';
-import { FC, LegacyRef } from 'react';
+import { FC, LegacyRef, useEffect } from 'react';
 
 interface MapBoxProps {
   mapNode: LegacyRef<HTMLDivElement> | null;
 }
 
-const MapBox: FC<MapBoxProps> = ({ mapNode }) => (
-  <Paper sx={{ height: 'calc(75vh)' }}>
-    <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
-  </Paper>
-);
+const MapBox: FC<MapBoxProps> = ({ mapNode }) => {
+  const handleClick = () => {
+    fetch(
+      'https://api.mapbox.com/geocoding/v5/mapbox.places/24.0228976,49.8360907.json?limit=1&types=region&access_token=pk.eyJ1IjoidHJvbnlhIiwiYSI6ImNpdXNteHUwdzAwMWkyenBmamRlbTk2Zm8ifQ.aFKT4IOHyPCPRt_GNfUYnw'
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <Paper sx={{ height: 'calc(75vh)' }}>
+      <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
+
+      <button id="geocoder" onClick={handleClick}>
+        test
+      </button>
+    </Paper>
+  );
+};
 
 export default MapBox;

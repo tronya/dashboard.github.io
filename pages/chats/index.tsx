e92f2f73b4f9425bbc18f0b1394eab7e21dccfb2
Nataliia Leshchak <n.leshchak@ualwo8689nbl.levi9.com>
2022-02-21 13:54:24 +0200

    add search input

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,5 +1,5 @@
 import { Grid, Paper } from '@mui/material';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useState } from 'react';
 import { UsersList } from '../../src/components/containers/UsersList/usersList.component';
 import Breadcrumbs from '../../src/components/ui/Breadcrumbs/breadcrumbs';
 import Loader from '../../src/components/ui/Loader/loader';
@@ -10,12 +10,15 @@ import useUsersGeolocation from '../../src/hooks/useUsersGeolocation';
 import { UserGeolocation } from '../../src/models/usersGeolocation';
 import EmptyChat from '../../src/components/containers/Chats/chat.empty';
 import { useRouter } from 'next/router';
+import SearchInput from '../../src/components/ui/SearchInput/searchInput';
 
 interface ChatPageProps {
   children?: ReactNode;
 }
 
 const ChatsPage: FC<ChatPageProps> = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   const { user: authUser } = useAuth();
   const usersGeolocation = useUsersGeolocation();
   const router = useRouter();
@@ -24,7 +27,7 @@ const ChatsPage: FC<ChatPageProps> = ({ children }) => {
     return <Loader />;
   }
 
-  const sortedUsers = usersGeolocation
+  const users = usersGeolocation
     .filter((user) => authUser.uid !== user.uid)
     .sort((a, b) =>
       a.displayName!! < b.displayName!!
@@ -32,6 +35,9 @@ const ChatsPage: FC<ChatPageProps> = ({ children }) => {
         : b.displayName!! > a.displayName!!
         ? 1
         : 0
+    )
+    .filter((person) =>
+      person.displayName!!.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
   const handleUserClick = (user: UserGeolocation) =>
@@ -43,8 +49,12 @@ const ChatsPage: FC<ChatPageProps> = ({ children }) => {
       <PageTitle title="Chats" />
       <Grid container sx={{ mt: 2, height: '75vh' }} spacing={2}>
         <Grid item xs={4} sm={3}>
+          <SearchInput
+            searchTerm={searchTerm}
+            onChangeSearchTerm={(searchStr) => setSearchTerm(searchStr)}
+          />
           <Paper sx={{ pt: 2, pl: 2, height: 1 }}>
-            <UsersList users={sortedUsers} onUserClick={handleUserClick} />
+            <UsersList users={users} onUserClick={handleUserClick} />
           </Paper>
         </Grid>
         <Grid item xs={8} sm={9}>
